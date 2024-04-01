47eea2dbbffbb37744a81c2ed8d3bdfb8dea926d
Testaccountforcon <164985645+Testaccountforcon@users.noreply.github.com>
2024-04-01 14:49:42 +0530

    Update and rename V1.1.1__initial_object.sql to V1.1.1__initial_object_v2.0.sql

--- migrations/V1.1.1__initial_object_v2.0.sql
+++ migrations/V1.1.1__initial_object_v2.0.sql
@@ -4,5 +4,11 @@ CREATE TABLE HELLO_WORLD
 (
    FIRST_NAME  VARCHAR
   ,LAST_NAME VARCHAR
+);
+
+create or replace table hello_world_2 
+(
+   FIRST_NAME  VARCHAR
+  ,LAST_NAME VARCHAR
    ,address varchar
 );
