f0a694079cf3431234561cdb5337006662b2f05f
Testaccountforcon <164985645+Testaccountforcon@users.noreply.github.com>
2024-04-26 16:48:42 +0530

    Update number_of_files_check.py

--- migrations/number_of_files_check.py
+++ migrations/number_of_files_check.py
@@ -1,26 +1,22 @@
 
 import pandas as pd
 import os
-
 from github import Github
-
 from ffcount import ffcount
-
 from github import GithubIntegration
-
-# Authentication is defined via github.Auth
 from github import Auth
+# Authentication is defined via github.Auth
+# Logging into GitHub repo using Authentication token
+
 b = os.environ['GT_auth_token']
 print(b)
-
 auth = Auth.Token(b)
-# First create a Github instance:
-
-# Public Web Github
 g = Github(auth=auth)
 for repo in g.get_user().get_repos():
     print(repo.name)
 
+# This function calculates the number of definitions in reference-schema against number of definitions in latest-schema-pull and stores the result in schemacheck.txt.
+# If number of definitions mismatch, a custom message is written in schemacheck.txt else the file is empty. 
 def count_files_in_directory(directory_path):
     num_files, _ = ffcount(directory_path)
     return num_files
