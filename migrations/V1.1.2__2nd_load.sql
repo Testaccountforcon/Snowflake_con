87ed9f8cd84e7af3fa8dea1a9e708b56eb73b528
Thilak-eb <tgopalakrishnan@employbridge.com>
2024-05-02 15:12:11 +0530

    Changes to be committed: 	modified:   migrations/V1.1.2__2nd_load.sql

--- migrations/V1.1.2__2nd_load.sql
+++ migrations/V1.1.2__2nd_load.sql
@@ -1,6 +1,6 @@
 use demo_db.public;
 
-create or replace view date_transform (date date) as (
+create or replace view date_transform as (
     select date_trunc('month', date) as month_of_date 
     from current_date
 );
