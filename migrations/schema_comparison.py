0afb591f360a4f7c364878aecc52c124748144fd
Testaccountforcon <164985645+Testaccountforcon@users.noreply.github.com>
2024-05-02 12:48:38 +0530

    Update schema_comparison.py

--- migrations/schema_comparison.py
+++ migrations/schema_comparison.py
@@ -133,52 +133,52 @@ for i in databases:
                      
 # This loop imports all the procedure definitions and store them in 'latest-schema-pull'
 # For each imported procedure, 'reference-schema-pull' is searched for the same table name and definitions are compared to ensure that the definitions match.
-for i in databases:
-    db_directory = i
-    sql = cur.execute(f"show schemas in {i}")
-    df = pd.DataFrame(cur.fetchall())
-    df.columns = [column[0] for column in cur.description]
-    print(df)
+# for i in databases:
+#     db_directory = i
+#     sql = cur.execute(f"show schemas in {i}")
+#     df = pd.DataFrame(cur.fetchall())
+#     df.columns = [column[0] for column in cur.description]
+#     print(df)
     
-    for j in df['name']:
-# Excluding the shared entities created by Snowflake by default as DDL commands can't be run on those.
-       if(j!='INFORMATION_SCHEMA'):
-        sc_directory = j
+#     for j in df['name']:
+# # Excluding the shared entities created by Snowflake by default as DDL commands can't be run on those.
+#        if(j!='INFORMATION_SCHEMA'):
+#         sc_directory = j
       
-        procedurespath = (parent_dir+ db_directory+sc_directory+'Procedures')
-        db_schema=i +"." + j
-        print (db_schema)
-        procedures_sql = cur.execute(f"show procedures in {db_schema}")
+#         procedurespath = (parent_dir+ db_directory+sc_directory+'Procedures')
+#         db_schema=i +"." + j
+#         print (db_schema)
+#         procedures_sql = cur.execute(f"show procedures in {db_schema}")
         
-        print(procedures_sql)
+#         print(procedures_sql)
 
-        if cur.rowcount!=0:
+#         if cur.rowcount!=0 and 'ASSOCIATE_SEMANTIC_CATEGORY':
             
-             procedures_df = pd.DataFrame(cur.fetchall())
-             procedures_df.columns = [column[0] for column in cur.description]
-             for k in procedures_df['name']:
-                   if(k!='SYSTEM$SEND_EMAIL'):
-                    path = (parent_dir+ db_directory+sc_directory+'Procedures'+k)
-                    db_schema_procedures=i+"."+j+"."+k
-                    definition_cursor= cur.execute(f"select get_ddl  ('procedure','{db_schema_procedures}');") 
-                    results=cur.fetchall()
-                    def_file_path=parent_dir+"/"+ db_directory + "/" +sc_directory + "/" + 'Procedures' + "/" +k + "/" + k +".txt"
-                    print(def_file_path)
-                    output="".join(results[0])
-                    print(output)
-                    repo.create_file(def_file_path, "message", output, branch="master")
-                    repo_name = g.get_user().get_repo("Snowflake_con")
-                    contents_1 = repo_name.get_contents(parent_dir+"/"+ db_directory + "/" +sc_directory + "/" + 'Procedures' + "/" +k + "/" + k +".txt")
-                    try:
-                     contents_2 = repo_name.get_contents("reference-schema"+"/"+ db_directory + "/" +sc_directory + "/" + 'Procedures' + "/" +k + "/" + k +".txt")
-                    except:
-                     a.append(parent_dir+"/"+ db_directory + "/" +sc_directory + "/" + 'Procedures' + "/" +k + "/" + k +".txt")
-                     continue
-                    if(contents_1.decoded_content.decode() == contents_2.decoded_content.decode()):
-                     print("match")
-                    else:
-                     a.append(parent_dir+"/"+ db_directory + "/" +sc_directory + "/" + 'Procedures' + "/" +k + "/" + k +".txt")
-                     print ("Error")
+#              procedures_df = pd.DataFrame(cur.fetchall())
+#              procedures_df.columns = [column[0] for column in cur.description]
+#              for k in procedures_df['name']:
+#                    if(k!='SYSTEM$SEND_EMAIL'):
+#                     path = (parent_dir+ db_directory+sc_directory+'Procedures'+k)
+#                     db_schema_procedures=i+"."+j+"."+k
+#                     definition_cursor= cur.execute(f"select get_ddl  ('procedure','{db_schema_procedures}');") 
+#                     results=cur.fetchall()
+#                     def_file_path=parent_dir+"/"+ db_directory + "/" +sc_directory + "/" + 'Procedures' + "/" +k + "/" + k +".txt"
+#                     print(def_file_path)
+#                     output="".join(results[0])
+#                     print(output)
+#                     repo.create_file(def_file_path, "message", output, branch="master")
+#                     repo_name = g.get_user().get_repo("Snowflake_con")
+#                     contents_1 = repo_name.get_contents(parent_dir+"/"+ db_directory + "/" +sc_directory + "/" + 'Procedures' + "/" +k + "/" + k +".txt")
+#                     try:
+#                      contents_2 = repo_name.get_contents("reference-schema"+"/"+ db_directory + "/" +sc_directory + "/" + 'Procedures' + "/" +k + "/" + k +".txt")
+#                     except:
+#                      a.append(parent_dir+"/"+ db_directory + "/" +sc_directory + "/" + 'Procedures' + "/" +k + "/" + k +".txt")
+#                      continue
+#                     if(contents_1.decoded_content.decode() == contents_2.decoded_content.decode()):
+#                      print("match")
+#                     else:
+#                      a.append(parent_dir+"/"+ db_directory + "/" +sc_directory + "/" + 'Procedures' + "/" +k + "/" + k +".txt")
+#                      print ("Error")
                
            
 
